2ba09b6d0d2b7ebffbb8903aaf289b092d371d66
sofiahui <so.huichulef@duocuc.cl>
2025-05-17 10:23:05 -0400

    add pedidomodel

---
+++ src/main/java/com/EcoMarketFullStack/EcoMarket/model/Pedido.java
@@ -0,0 +1,15 @@
+package com.EcoMarketFullStack.EcoMarket.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="pedido")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+}
