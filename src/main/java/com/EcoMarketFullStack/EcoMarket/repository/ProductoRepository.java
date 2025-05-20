7e88a37007ca1ba02972495d8318ad02397faed8
mmontes2 <mmontes@s-th.cl>
2025-05-20 18:23:07 -0400

    FIX: ProductRepository Interface Modification

--- src/main/java/com/EcoMarketFullStack/EcoMarket/repository/ProductoRepository.java
+++ src/main/java/com/EcoMarketFullStack/EcoMarket/repository/ProductoRepository.java
@@ -1,7 +1,6 @@
 package com.EcoMarketFullStack.EcoMarket.repository;
-
+// FIX INTERFACE
 import com.EcoMarketFullStack.EcoMarket.model.Producto;
-import com.EcoMarketFullStack.EcoMarket.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
