07cc540de58bba1c64ab1baf4360c562e55c5b7e
mmontes2 <mmontes@s-th.cl>
2025-05-21 19:26:14 -0400

    Fix:  Modified Product class, renamed table

--- src/main/java/com/EcoMarketFullStack/EcoMarket/model/Producto.java
+++ src/main/java/com/EcoMarketFullStack/EcoMarket/model/Producto.java
@@ -6,11 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+
 import java.util.List;
 
 @Entity
-@Table(name="product")
+@Table(name="producto") 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
